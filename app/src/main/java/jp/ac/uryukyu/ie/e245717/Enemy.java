7f5447ae211a71974bfb01040e053956e0061357
FUMA <e245717@ie.u-ryukyu.ac.jp>
2024-11-19 21:06:41 +0900

    caps_correctioned_rep3

--- app/src/main/java/jp/ac/uryukyu/ie/e245717/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245717/Enemy.java
@@ -9,10 +9,38 @@ package jp.ac.uryukyu.ie.e245717;
  * Created by tnal on 2016/11/13.
  */
 public class Enemy {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getHitPoint(){
+        return hitPoint;
+    }
+    public void setHitPoint(int hitPoint) {
+        this.hitPoint = hitPoint;
+    }
+    public int getAttack(){
+        return attack;
+    }
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+    public boolean getDead(){
+        return dead;
+    }
+    public void setDead(boolean dead) {
+        this.dead = dead;
+    }
+
+
+    
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -38,7 +66,7 @@ public class Enemy {
             return;
         }
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
         hero.wounded(damage);
         
     }
